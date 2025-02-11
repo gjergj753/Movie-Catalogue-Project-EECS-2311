cc59331eb38abd1c074f661c40e303236ab04346
rythemg5 <rythemg5@my.yorku.ca>
2025-02-11 12:31:02 -0500

    Created Controllers and FXML files

---
+++ src/main/controller/Client/ClientController.java
@@ -0,0 +1,9 @@
+package main.controller.Client;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ClientController {
+	
+}
