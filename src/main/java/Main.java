d69765435a09c07cf4d16aedd40dd50c6ef4c314
rythemg5 <rythemg5@my.yorku.ca>
2025-02-12 14:39:36 -0500

    minor folder change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application{
 	@Override
 	public void start(Stage stage) throws Exception {
 		try {
-		FXMLLoader fxmlLoader=new FXMLLoader(getClass().getResource("/Fxml/Login.fxml"));
+		FXMLLoader fxmlLoader=new FXMLLoader(getClass().getClassLoader().getResource("/Fxml/Login.fxml"));
 		Scene scene=new Scene(fxmlLoader.load());
 		stage.setScene(scene);
 		stage.show();
