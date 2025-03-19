987865b111f3f41d9cc825932d41bf4172852208
Sarah Usher <90471566+sarahjessica21@users.noreply.github.com>
2025-03-19 17:42:31 -0400

    Add files via upload
    I've updated User.java to include lists for watched, plan to watch, dropped, and favorite movies. It also includes methods to add/remove movies and ensures persistence.

---
+++ User.java
@@ -0,0 +1,62 @@
+package model;
+
+import java.io.*;
+import java.util.*;
+
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    private static int id = 1;
+    private int userId;
+    private String userName;
+    private String password;
+    private static final String FILE_NAME = "users.dat";
+    private static Map<String, User> users = loadUsers();
+
+    // Movie lists
+    private List<Movie> watchedMovies = new ArrayList<>();
+    private List<Movie> planToWatchMovies = new ArrayList<>();
+    private List<Movie> droppedMovies = new ArrayList<>();
+    private List<Movie> favoriteMovies = new ArrayList<>();
+
+    public User(String userName, String password) {
+        this.userId = id++;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    // Getters
+    public List<Movie> getWatchedMovies() { return watchedMovies; }
+    public List<Movie> getPlanToWatchMovies() { return planToWatchMovies; }
+    public List<Movie> getDroppedMovies() { return droppedMovies; }
+    public List<Movie> getFavoriteMovies() { return favoriteMovies; }
+
+    // Methods to add movies to lists
+    public void addWatchedMovie(Movie movie) { if (!watchedMovies.contains(movie)) watchedMovies.add(movie); }
+    public void addPlanToWatchMovie(Movie movie) { if (!planToWatchMovies.contains(movie)) planToWatchMovies.add(movie); }
+    public void addDroppedMovie(Movie movie) { if (!droppedMovies.contains(movie)) droppedMovies.add(movie); }
+    public void addFavoriteMovie(Movie movie) { if (!favoriteMovies.contains(movie)) favoriteMovies.add(movie); }
+
+    // Methods to remove movies from lists
+    public void removeWatchedMovie(Movie movie) { watchedMovies.remove(movie); }
+    public void removePlanToWatchMovie(Movie movie) { planToWatchMovies.remove(movie); }
+    public void removeDroppedMovie(Movie movie) { droppedMovies.remove(movie); }
+    public void removeFavoriteMovie(Movie movie) { favoriteMovies.remove(movie); }
+
+    // Save users
+    public static void saveUsers() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(users);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Load users
+    private static Map<String, User> loadUsers() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            return (Map<String, User>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            return new HashMap<>();
+        }
+    }
+}
